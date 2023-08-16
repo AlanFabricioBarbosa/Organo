3ced05c1e79c52f7ed1f7182c6cb175bd4f83905
Alan Fabrício Barbosa da Silva <alanfabriciobarbosa1@gmail.com>
2023-08-16 19:06:06 -0300

    Formulario concluido

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,14 +1,35 @@
+import Button from '../Button';
+import DropDown from '../DropDown';
 import TextArea from '../TextArea';
 import './Form.css'
 
 const Form = () => {
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'Ux e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const saving = (event) => {
+        event.preventDefault()
+        console.log("Formulario foi submetido")
+    }
+
     return (
         <section className='form'>
-            <form>
+            <form onSubmit={saving}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <TextArea label="Nome" placeholder="Digite seu nome"/>
-                <TextArea label="Cargo" placeholder="Digite o seu cargo"/>
+                <TextArea required={true} label="Nome" placeholder="Digite seu nome"/>
+                <TextArea required={true} label="Cargo" placeholder="Digite o seu cargo"/>
                 <TextArea label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <DropDown required={true} label="Time" itens={times}/>
+                <Button>
+                    Criar Card
+                </Button>
             </form>
         </section>
     )
